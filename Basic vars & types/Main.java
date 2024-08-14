5b459aef9f85ef697950347ddda57422bd922c0c
Adrien B <bourdeaux.adrien@gmail.com>
2024-08-13 21:31:03 -0400

    Basic vars added

---
+++ Basic vars & types/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args){
+        String name = "Ooooooohm";
+        System.out.println("Hello " + name);
+        char initial = name.charAt(0);
+        System.out.println("Your first initial is " + initial);
+        int temp = 19;
+        System.out.println("The current temperature is " + temp + "C");
+        boolean life = true;
+        System.out.println("You being alive is a " + life + " statement");
+        float pi = 3.14f;
+        int diamater = 6;
+        System.out.println("The circumference of a circle where the diamater is " + diamater + " is " + pi*diamater);
+    }
+}
